0809b8a8f8dd43f86f23ec31ecd0f81f7b5eab85
yazin123 <mohammedyazin4@gmail.com>
2024-12-21 15:13:57 +0530

    first demo

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Brands from '@/components/Brands';
 import CarCollection from '@/components/CarCollection';
 import Footer from '@/components/Footer';
 import { Hero } from '@/components/Hero';
-import HowItWorks from '@/components/HowitWorks';
+import HowItWorks from '@/components/HowItWorks';
 import LocationSearch from '@/components/LocationSearch';
 import ServicesSection from '@/components/ServiceSection';
 import TestimonialSlider from '@/components/TestimonialSlider';
