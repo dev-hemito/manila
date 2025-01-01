f661a240aad2150042f5d9988d77039345fb9341
Dev Hemito <dev.hemito@gmail.com>
2025-01-01 20:12:59 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -6,12 +6,14 @@ import HowItWorks from '@/components/HowItWorks';
 import LocationSearch from '@/components/LocationSearch';
 import ServicesSection from '@/components/ServiceSection';
 import TestimonialSlider from '@/components/TestimonialSlider';
+import FloatingButtons from '@/components/FloatingButtons';
 import React from 'react';
 
 const Page = () => {
   return (
    <>
    <Hero/>
+    <FloatingButtons/>
    <LocationSearch/>
     <Brands/>
     <CarCollection/>
